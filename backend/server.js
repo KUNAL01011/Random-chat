e502d0f213db6408db33824afae8097c43e9ffa6
Kunal Kumar <kunal34255@gmail.com>
2024-07-12 15:57:00 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import authRouter from './routes/auth.route.js';
@@ -11,6 +12,8 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
@@ -18,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRouter);
 app.use("/api/messages",messageRouter);
 app.use("/api/users",userRouter);
+
+app.use(express.static(path.join(__dirname,'/frontend/dist')));
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,'frontend',"dist",'index.html'));
+})
 
 
 server.listen(PORT,() => {
