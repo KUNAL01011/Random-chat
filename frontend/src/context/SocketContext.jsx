8dae62fbc3e4153eed40297cc80c436484227a8e
Kunal Kumar <kunal34255@gmail.com>
2024-07-12 16:38:01 +0530

    url changed

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:8000",{
+      const socket = io("https://web-chat-94zg.onrender.com",{
         query:{
             userId: authUser._id
         }
